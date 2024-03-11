d0b605f801e3ebaf099b1109b9671b37cb822929
Alex Richard <alexrichard_lutador@outlook.com>
2024-03-11 11:57:53 -0300

    feat: Introdução ao Java
    Escrita da classe básica Main do java
    Escrita do fonte básico do C
    Definição de variaveis primitivas no java
    
    Ref.: CAP-0003

---
+++ src/java/br/com/intro/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String args[])
+	{
+		int numero= 0;
+		double decimal= 0.5;
+		float precisao= 0.85f;
+		char letra= 'a';
+		boolean estado = true;
+	
+		System.out.println("hello World" + numero+ decimal + precisao + letra + estado);
+	}
+}
